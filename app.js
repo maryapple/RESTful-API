0c35616e001e656af891996e4031569e39176562
maryapple <maria9ku@gmail.com>
2019-11-05 20:39:27 +0300

    .gitignore added

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const app = new express()
+const mongoose = require('mongoose')
+require('dotenv/config')
 
+// import routes
+const postsRoute = require('./routes/posts')
+
+app.use('/posts', postsRoute)
 
 // ROUTES
 app.get('/', (req, res) => {
     res.send('We are on home')
 })
-app.get('/posts', (req, res) => {
+
+/* app.get('/posts', (req, res) => {
     res.send('We are on posts')
-})
+}) */
+
+
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true},
+    () => console.log('conncted to DB')
+)
 
 // Listening to the server
 app.listen(3000)
